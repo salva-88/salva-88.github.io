2033980f483cba7a6903eb3bd5f73ef9973dc586
salva-88 <fathimasalva37@gmail.com>
2025-10-14 11:16:23 +0530

    portfolio-added

---
+++ portfolio/js/script.js
@@ -0,0 +1,6 @@
+$(document). ready(function(){
+    $('.toggle-bt').click(function(){
+        $('.toggle-bt').toggleClass('toggled');
+         $('.header-cnt nav ul').toggleClass('toggled');
+    });
+})
